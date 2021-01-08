cd1e3d23b6da518a4d582fa53b8b898842a04e23
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-01-08 19:46:04 +0200

    USART

---
+++ Interfacing/USART/Exercise1/Exercise1.c
@@ -0,0 +1,23 @@
+/*
+ * Exercise1.c
+ *
+ * Created: 4/10/2014 9:23:41 PM
+ * Author: Ahmed Sabry
+ */ 
+
+#include "uart.h"
+
+int main(void)
+{
+	uint8 Str[20];
+	uint8 data;
+	UART_init();
+    while(1)
+    { 
+		/* data = UART_recieveByte(); // Receive Byte from Terminal1 */
+		/* UART_sendByte(data);  // Resend the received byte to Terminal2 */
+		
+		UART_receiveString(Str); /* Receive String from Terminal */
+		UART_sendString(Str); /* Resend the string to Terminal2 */
+    }
+}
