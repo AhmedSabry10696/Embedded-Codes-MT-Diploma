1724598a7e3c8e43fcef27ef28299eae1d7ba759
AhmedSabry10696 <ahmed.sabry10696@gmail.com>
2021-01-08 21:16:39 +0200

    SPI

---
+++ Interfacing/SPI/Challenge_MC1/MC1.c
@@ -0,0 +1,25 @@
+/*
+ * MC1.c
+ *
+ *  Created on: Sep 18, 2015
+ *  Author: Ahmed Sabry
+ */
+
+#include "keypad.h"
+#include "spi.h"
+
+int main(void)
+{
+	uint8 key;
+	SPI_initMaster(); /* intitialize Master MC */
+    
+	while(1)
+    {
+		key = KeyPad_getPressedKey(); /* get the pressed key */
+		SPI_sendByte(key); /* send the pressed key to the second MC using spi */
+		
+		_delay_ms(500); /* press time is 500msec */
+    }
+}
+
+
---
+++ Interfacing/SPI/Exercise2_MC1/MC1.c
@@ -0,0 +1,23 @@
+/*
+ * MC1.c
+ *
+ *  Created on: Sep 18, 2015
+ *  Author: Ahmed Sabry
+ */
+
+#include "spi.h"
+
+int main(void)
+{
+	SPI_initMaster(); /* Master Initialization */
+	_delay_ms(50); /* delay until MC2 finish its initialization task */
+	
+    SPI_SendString("I am Micro1#");
+    
+    while(1)
+    {
+        /* do nothing */
+    }
+}
+
+
